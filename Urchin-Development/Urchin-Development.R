8eeb88f2da6a15b087edab1e692977bd5643cb80
Ross Whippo <rosspitality@gmail.com>
2019-01-18 09:12:59 -0800

    Added development script and updated algal culture
    Visualizations of urchin development, added food in gut column to development spreadsheet. Added latest algal culture.

---
+++ Urchin-Development/Urchin-Development.R
@@ -0,0 +1,68 @@
+###################################################################################
+#                                                                                ##
+# Urchin Development                                                             ##
+# Data are current as of 2019-01-18                                              ##
+# Data source: Oregon Institute of Marine Biology                                ##
+# R code prepared by Ross Whippo                                                 ##
+# Last updated 2019-01-18                                                        ##
+#                                                                                ##
+###################################################################################
+
+# SUMMARY:
+# Code to visualize and analyse growth patterns of larval Strongylocentrotus
+# purpuratus for Ross Whippo's winter 2019 lab rotation with Richard Emlet.
+
+# Required Files (check that script is loading latest version):
+# Urchin-Temp-Food-Development.csv
+
+# Associated Scripts:
+# None
+
+# TO DO
+
+###################################################################################
+# TABLE OF CONTENTS                                                               #
+#                                                                                 #
+# RECENT CHANGES TO SCRIPT                                                        #
+# LOAD PACKAGES                                                                   #
+# READ IN AND PREPARE DATA                                                        #
+# MANIPULATE DATA                                                                 #   
+#                                                                                 #
+###################################################################################
+
+###################################################################################
+# RECENT CHANGES TO SCRIPT                                                        #
+###################################################################################
+
+# 2019-01-18 Script created.
+
+###################################################################################
+# LOAD PACKAGES                                                                   #
+###################################################################################
+
+library(tidyverse)
+library(ggplot2)
+library(viridis)
+
+###################################################################################
+# READ IN AND PREPARE DATA                                                        #
+###################################################################################
+
+devel <- read.csv("Urchin-Temp-Food-Development.csv")
+
+str(devel)
+
+###################################################################################
+# MANIPULATE DATA                                                                 #
+###################################################################################
+
+ggplot(data = filter(devel, date != "2019-01-11"), aes(x = date, y = totalLength, fill = temperatureTreatment)) +
+  geom_boxplot() +
+  facet_wrap(~ foodTreatment) +
+  scale_fill_viridis(discrete = TRUE, begin = 0.25, end = 0.95, option = "D'")
+  
+
+############### SUBSECTION HERE
+
+#####
+#<<<<<<<<<<<<<<<<<<<<<<<<<<END OF SCRIPT>>>>>>>>>>>>>>>>>>>>>>>>#
